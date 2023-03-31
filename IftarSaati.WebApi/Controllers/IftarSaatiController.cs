aed1c10424cafc21f2ceaf0d06de0ea2a8330cec
Neslihan Çakırbaş <DL_neslihan@outlook.com>
2023-03-31 11:32:39 +0300

    initial commit

---
+++ IftarSaati.WebApi/Controllers/IftarSaatiController.cs
@@ -0,0 +1,48 @@
+﻿using DataProvider.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+
+namespace IftarSaati.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IftarSaatiController : ControllerBase
+    {
+
+        public string jsonData;
+        public static List<Root> infoStorage = new List<Root>();
+        private string apiKey = "your api key";
+
+
+
+        [HttpGet]
+        public IActionResult GetTime()
+        {
+            return Ok(infoStorage);
+        }
+
+        [HttpPost]
+        public IActionResult GetSpesificTime(string city)
+        {
+            var client = new RestClient("https://api.collectapi.com");
+            var request = new RestRequest($"pray/single?ezan=Ak%C5%9Fam&data.city={city}", Method.Get);
+            request.AddHeader("authorization", apiKey);
+            request.AddHeader("content-type", "application/json");
+            RestResponse response = client.Execute(request);
+
+            jsonData=response.Content;
+            Root infos = JsonConvert.DeserializeObject<Root>(jsonData);
+            infoStorage.Add(infos);
+
+            return Ok(infos);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ ProvideData/Dtos/Informations.cs
@@ -0,0 +1,18 @@
+﻿namespace DataProvider.Dtos
+{
+    public class Informations
+    {
+        public string time { get; set; }
+        public string hour { get; set; }
+        public string min { get; set; }
+    }
+
+    public class Root
+    {
+        public bool success { get; set; }
+        public List<Informations> result { get; set; }
+    }
+
+
+
+}
